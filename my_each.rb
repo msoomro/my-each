5f59d4dc25744d6db7e71fbba4109c35f32dd3e4
Moomal <moomalmsoomro@gmail.com>
2020-11-22 22:22:35 -0500

    commit tree f031c599aa4d9255cb86bf87968be5a9d8d7060a

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+# requires an array/list 
+def my_each (list)
+  i = 0
+
+  while i < list.size
+    yield(list[i])
+    print list[i]
+    i += 1
+  end
+
+  list
 end
+
